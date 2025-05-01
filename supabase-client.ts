8f0327cb5ef014b8ebbf3a226ef9ad5997539f8a
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-01 13:43:00 -0500

    supabase client contexion

---
+++ supabase-client.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASEURL = import.meta.env.VITE_SUPABASE_URL; // URL de Supabase
+const SUPABASEKEY = import.meta.env.VITE_SUPABASE_KEY; // API Key de Supabase
+
+export const supabase = createClient(SUPABASEURL, SUPABASEKEY);
