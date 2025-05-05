ec0bc63af9a71b00ad0d8df0ea0503bd918e4bc8
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-05 16:06:34 -0500

    grid

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,15 @@
 import Header from "../ui/Header";
-
+import PresentesHoyCard from "../ui/PresentesHoyCard";
+import AusentesHoyCard from "../ui/AusentesHoyCard";
 function Dashboard() {
   return (
-    <div className="bg-gray-100 w-screen h-screen flex relative items-center p-20 overflow-hidden ">
+    <div className="bg-gray-50 w-screen min-h-screen overflow-hidden">
       <Header pageTitle="Dashboard" />
+
+      <div className="px-9 py-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <PresentesHoyCard />
+        <AusentesHoyCard />
+      </div>
     </div>
   );
 }
