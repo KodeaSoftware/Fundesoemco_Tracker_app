10d5d5f7e49b11c6e1fe6aea540d7f6ca1202aea
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-05 12:26:26 -0500

    fogtor-password

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,14 @@
+function ForgotPassword() {
+  return (
+    <div>
+      <h1>Forgot Password</h1>
+      <p>Please enter your email address to reset your password.</p>
+      <form>
+        <input type="email" placeholder="Email" required />
+        <button type="submit">Send Reset Link</button>
+      </form>
+    </div>
+  );
+}
+
+export default ForgotPassword;
