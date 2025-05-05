7a762262ac51eb12fb6f49409019d4729f8516b1
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-04 23:01:32 -0500

    Router, AppRouter.jsx and App.jsx whit routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <Router>
+      <AppRoutes />
+    </Router>
   );
 }
 
