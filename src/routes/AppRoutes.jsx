75056b609107ac724097e7881dcaed13a0253fd0
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-05 12:24:58 -0500

    dashboard route and forgot-password

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
+import ForgotPassword from "../pages/ForgotPassword";
+import Dashboard from "../pages/Dashboard";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/" element={<Dashboard />} />
     </Routes>
   );
 };
