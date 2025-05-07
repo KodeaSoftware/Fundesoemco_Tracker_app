d7933b2c1dfa5162d47a37599b6b226abd399466
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-06 23:52:42 -0500

    animation added

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,16 +3,21 @@ import Login from "../pages/Login";
 import ForgotPassword from "../pages/ForgotPassword";
 import Dashboard from "../pages/Dashboard";
 import Projects from "../pages/Projects";
-import CreateProject from "../pages/CreateProject";
+import CreateProject from "../ui/CreateProject";
+import { AnimatePresence } from "motion/react";
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/project-list" element={<Projects />} />
-    </Routes>
+    <AnimatePresence>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        <Route path="/" element={<Dashboard />} />
+
+        <Route path="/project-list" element={<Projects />} />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
