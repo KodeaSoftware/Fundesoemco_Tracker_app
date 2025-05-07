6a478f0bd6d76a87516089c94c8530f9016cdace
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-07 08:45:19 -0500

    Employees page

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,15 @@
+// Layout
+import PageLayout from "../layout/PageLayout";
+
+// Componentes
+import Header from "../ui/Header";
+
+function Employees() {
+  return (
+    <PageLayout>
+      <Header pageTitle="Lista de empleados" />
+    </PageLayout>
+  );
+}
+
+export default Employees;
