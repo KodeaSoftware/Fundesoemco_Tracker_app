e3bd94b720ef19c3b46a21de0c4c9c7b750b2a07
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-10 00:27:21 -0500

    motion layer

---
+++ src/motion/modalAnimation.js
@@ -0,0 +1,18 @@
+export const modalAnimationParams = {
+  initial: {
+    opacity: 0,
+    scale: 0.96,
+    y: 16,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.95,
+    y: 12,
+    duration: 0,
+  },
+};
