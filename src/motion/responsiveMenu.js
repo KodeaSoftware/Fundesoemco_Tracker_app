68212673297c3e551fba91726bdca6bc4922bfd3
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-14 11:37:22 -0500

    animation

---
+++ src/motion/responsiveMenu.js
@@ -0,0 +1,6 @@
+export const responsiveMenuParams = {
+  initial: { opacity: 1, x: -50 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: 0 },
+  transition: { duration: 0.2, ease: "easeOut" },
+};
