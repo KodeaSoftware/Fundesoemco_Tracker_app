283f3e4657f7dfdf68486e63841998abdb51fbbb
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-14 11:36:41 -0500

    fix dashboard layer

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -19,7 +19,7 @@ function Dashboard() {
     <PageLayout>
       <Header pageTitle="Dashboard" />
       <motion.div {...pageAnimationsParams}>
-        <div className="px-9 py-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pb-10">
+        <div className="px-9 sm:py-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pb-10 ">
           <PresentesHoyCard />
           <AusentesHoyCard />
           <HorasPromedioCard />
