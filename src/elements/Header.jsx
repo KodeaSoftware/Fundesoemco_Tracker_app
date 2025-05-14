4a3fa497a7c766b2408f0f96435a60e4e1b7aeff
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-05-14 11:36:14 -0500

    fix header

--- src/elements/Header.jsx
+++ src/elements/Header.jsx
@@ -14,14 +14,64 @@ import { FiUserPlus } from "react-icons/fi";
 import { LuUserCog } from "react-icons/lu";
 import { RxDashboard } from "react-icons/rx";
 import { GoProjectRoadmap } from "react-icons/go";
+import { IoMenuSharp } from "react-icons/io5";
+import { useState } from "react";
+import ResponsiveMenu from "./ResponsiveMenu";
+
+// motion animaciones
+// eslint-disable-next-line no-unused-vars
+import { motion, AnimatePresence } from "motion/react";
 
 function Header(props) {
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const closeMenu = () => setDisplayMenu(false);
+
+  const handleResponsiveMenu = () => {
+    setDisplayMenu(true);
+  };
   return (
     <>
+      <header className="w-full m-0 h-20 block pl-8 pr-8  sm:hidden p-4 flex items-center justify-between">
+        <button
+          className="w-[50px] h-[50px]  cursor-pointer bg-white flex items-center justify-center text-2xl border-1 rounded-md"
+          onClick={handleResponsiveMenu}
+        >
+          <IoMenuSharp />
+        </button>
+        <h1 className="text-md ml-4 rounded-sm  p-2 pl-4 pr-4 font-semibold border-1 bg-white ">
+          {props.pageTitle}
+        </h1>
+      </header>
+      <AnimatePresence>
+        {displayMenu && (
+          <motion.div
+            className="fixed inset-0 bg-black/40 z-40"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            onClick={closeMenu}
+          >
+            <motion.div
+              className="absolute top-0 left-0 bg-red-200 w-[300px] h-screen z-50"
+              initial={{ x: "-100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+              transition={{ duration: 0.1, ease: "easeOut" }}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <ResponsiveMenu pageTitle={props.pageTitle} />
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
       <header
-        className=" h-15 flex items-center gap-5 font-[Noto sans]  px-4 absolute top-0 z-10 
+        className="  hidden sm:block sm:h-15 sm:flex sm:items-center sm:gap-5 sm:font-[Noto sans]  sm:px-4 sm:absolute top-0 z-10 
      left-0 right-0 
-     bg-white w-full border"
+     bg-white w-full border
+   
+     "
       >
         <img
           src="fundesoemco-logo.png"
